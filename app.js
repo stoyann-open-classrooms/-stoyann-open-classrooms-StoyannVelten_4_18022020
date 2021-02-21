30853fe1226a894ab2a31bad140d71388d0230c8
Stoyann <stoyann.openclassrooms@gmail.com>
2021-02-21 23:15:59 +1100

    refonte du code

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const modalBtn = document.querySelector(".hero__submit");
-const modalBg = document.querySelector(".modal");
 
-// launch modal
-modalBtn.addEventListener('click', function() {
-    modalBg.classList.add("bg-active");
-})
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav__links');
+    
+
+
+//    toggle nav
+   burger.addEventListener("click", () => {
+        nav.classList.toggle('nav__active');
+    });  
+
+}
+navSlide();
+
+
+
+
+
+
+
+
+
+
